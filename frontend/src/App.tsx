4efd1661e3519ba755e62256ad026d63c220b58f
amirrajj-dev <amiramraja@gmail.com>
2025-04-28 23:18:02 +0330

    added some of the project necessary routes⚡👺

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import SignUp from "./pages/auth/signup/SignUp"
+import SignIn from "./pages/auth/signin/SignIn"
+import ForgotPassword from "./pages/auth/forgot-password/ForgotPassword"
+import ResetPassword from "./pages/auth/reset-password/ResetPassword"
+import Home from "./pages/home/Home"
 
 const App = () => {
   return (
-    <div className=''>
-
-      <button className='btn btn-success btn-soft m-4'>Click Me</button>
+    <div className='p-4 transition-colors duration-200'>
+      <Routes>
+      <Route path="/" element={<Home/>}></Route>
+        <Route path="/auth/signup" element={<SignUp/>}></Route>
+        <Route path="/auth/signin" element={<SignIn/>}></Route>
+        <Route path="/auth/forgot-password" element={<ForgotPassword/>}></Route>
+        <Route path="/auth/reset-password" element={<ResetPassword/>}></Route>
+      </Routes>
     </div>
   )
 }
