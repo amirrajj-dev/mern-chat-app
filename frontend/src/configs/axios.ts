b9adc0ed8309c3fc40c2bb8ecc473511175cf2d4
amirrajj-dev <amiramraja@gmail.com>
2025-05-11 20:31:13 +0330

    necessary deploy to render change⚡👺

--- frontend/src/configs/axios.ts
+++ frontend/src/configs/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
-
+// import.meta.env.VITE_FRONTEND_URL || "http://localhost:5000"
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_FRONTEND_URL || "http://localhost:5000",
+    baseURL : 'https://mern-chat-app-pvv3.onrender.com',
     withCredentials : true
 })
